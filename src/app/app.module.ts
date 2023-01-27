08ca62a3486b1f1ee2f7270a38e553cf12bd1e38
Andersondev429 <anderson-429@hotmail.com>
2023-01-27 19:03:44 -0300

    Steps concluidos

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { PersonalInfoComponent } from './formulario/personal-info/personal-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent,
+    PersonalInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
